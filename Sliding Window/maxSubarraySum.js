c66c148fd4b904bd7a7669347253ba893adbe472
조아 <parkjoah@naver.com>
2025-11-11 21:46:45 +0900

    [도전과제] 다중포인터 패턴 - maxSubarraySum

--- Sliding Window/maxSubarraySum.js
+++ Sliding Window/maxSubarraySum.js
@@ -13,3 +13,19 @@ function maxSubarraySum(arr, num) {
 }
 
 //→ 첫번째 전체 sum을 구하고, 빼고 더하는식으로…….
+
+function maxSubarraySum(arr, num) {
+  if (arr.length < num) return null;
+  let sum = 0;
+  for (let i = 0; i < num; i++) {
+    sum += arr[i];
+  }
+  let maxSum = sum;
+  for (let j = 1; j < arr.length - num + 1; j++) {
+    sum = sum - arr[j - 1] + arr[j + num - 1];
+    if (sum > maxSum) {
+      maxSum = sum;
+    }
+  }
+  return maxSum;
+}
