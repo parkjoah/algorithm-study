edf0af7be008b5cb27ae666fe986abd6dafe2d8a
조아 <parkjoah@naver.com>
2025-11-30 03:10:39 +0900

    단일 연결 리스트 - Node : push Method

---
+++ SinglyLinkedLists /Node.js
@@ -0,0 +1,40 @@
+// piece of data - val
+// reference to next node - next
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+// 안좋은 예시.
+var first = new Node("Hi");
+first.next = new Node("there");
+first.next.next = new Node("how");
+first.next.next.next = new Node("are");
+first.next.next.next.next = new Node("you");
+
+class SinglyLinkedList {
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+var list = new SinglyLinkedList();
+list.push("HELLO");
+list.push("GOODBYE");
