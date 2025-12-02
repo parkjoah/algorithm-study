11b5335eb214bb6c154210b096211683386ac60e
조아 <parkjoah@naver.com>
2025-12-02 22:48:26 +0900

    단일 연결 리스트 - Node : reverse Method

--- SinglyLinkedLists /Node.js
+++ SinglyLinkedLists /Node.js
@@ -107,6 +107,29 @@ class SinglyLinkedList {
     this.length--;
     return removed;
   }
+  print() {
+    var arr = [];
+    var current = this.head;
+    while (current) {
+      arr.push(current.val);
+      current = current.next;
+    }
+    console.log(arr);
+  }
+  reverse() {
+    var node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    var next;
+    var prev = null;
+    for (var i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
 }
 
 var list = new SinglyLinkedList();
