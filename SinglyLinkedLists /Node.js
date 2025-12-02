b14d4629785c0d6f073e89e42ee72ecdf1950b7f
조아 <parkjoah@naver.com>
2025-12-02 13:43:07 +0900

    단일 연결 리스트 - Node : shift Method

--- SinglyLinkedLists /Node.js
+++ SinglyLinkedLists /Node.js
@@ -52,6 +52,17 @@ class SinglyLinkedList {
     }
     return current;
   }
+  shift() {
+    if (!this.head) return undefined;
+    var currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return currentHead;
+  }
 }
 
 var list = new SinglyLinkedList();
